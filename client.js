85a948b5647727edc27e2ca6ad0309177020197a
pftiller <pftiller@gmail.com>
2017-12-18 09:49:58 -0600

    Adding additional inputs

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 $(document).ready(readyNow);
 
+var averageSalary = 0;
+
 function readyNow() {
   console.log('JQ');
 $('#submitBtn').on('click', submitClick);
@@ -9,4 +11,6 @@ function submitClick() {
   var salary = $('#salary').val();
   console.log('button clicked:', salary);
   $('#output').append('<p>' + salary + '</p>');
+  averageSalary += salary / 12;
+  $('#averageSalary').html('<strong>' + averageSalary + '</strong>');
 }
