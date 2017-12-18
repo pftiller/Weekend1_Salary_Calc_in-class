4cab41056c1743fdadeeb573f87e7a4ae362189e
pftiller <pftiller@gmail.com>
2017-12-18 10:32:25 -0600

    Add delete button

--- client.js
+++ client.js
@@ -5,12 +5,23 @@ var averageSalary = 0;
 function readyNow() {
   console.log('JQ');
 $('#submitBtn').on('click', submitClick);
+$('#output').on('click', '.delete', deleteRow);
 }
 
 function submitClick() {
   var salary = $('#salary').val();
+  var row = $('<tr></tr>');
+  row.append('<td>' + $('#first').val() + '</td>');
+  row.append('<td>' + $('#last').val() + '</td>');
+  row.append('<td>' + $('#title').val() + '</td>');
+  row.append('<td>' + $('#salary').val() + '</td>');
+  row.append('<td>' + $('#empID').val() + '</td>');
+  row.append('<td><button class="delete">Delete</button></td>');
   console.log('button clicked:', salary);
-  $('#output').append('<p>' + salary + '</p>');
+  $('#output').append(row);
   averageSalary += salary / 12;
   $('#averageSalary').html('<strong>' + averageSalary + '</strong>');
+}
+function deleteRow(){
+  $(this).closest('tr').remove();
 }
